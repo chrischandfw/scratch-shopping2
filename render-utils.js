bae303165e1fb55fb7c840234ef9043cd65e37f3
chris_chan <chrischandfw@gmail.com>
2022-03-29 15:50:52 -0500

    set variable to item and amount to start rendering item from list

--- render-utils.js
+++ render-utils.js
@@ -1,14 +1,17 @@
 export function renderItem(item) {
     const renderItemEl = document.createElement('div');
-    const pTagEl = document.createElement('p');
+    const itemName = document.createElement('p');
+    const itemAmount = document.createElement('p');
+	
     // depending on whether the todo is complete, give the div the appropriate css class ('complete' or 'incomplete')
     renderItemEl.classList.add(item.is_bought ? 'complete' : 'incomplete');  
     // add the 'todo' css class no matter what
     renderItemEl.classList.add('item');
     // put the todo's text into the p tag
-    pTagEl.textContent = item.item;
+    itemName.textContent = item.item;
+    itemAmount.textContent = item.amount;
     // append stuff
-    renderItemEl.append(pTagEl);
+    renderItemEl.append(itemName, itemAmount);
     // return the div
     return renderItemEl;
 }
