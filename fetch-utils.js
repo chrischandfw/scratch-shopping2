1ea1801f9b5e44f836d72e5f4c79b519d8542d09
chris_chan <chrischandfw@gmail.com>
2022-03-29 14:36:14 -0500

    copied and paste supabase api again

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,4 +1,7 @@
-const SUPABASE_URL = '"https://wkkubdeijvrntdmunqer.supabase.co"';
+/* eslint-disable no-console */
+
+const SUPABASE_URL = 'https://wkkubdeijvrntdmunqer.supabase.co';
+
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indra3ViZGVpanZybnRkbXVucWVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc1NTM1MTcsImV4cCI6MTk2MzEyOTUxN30.qE-NCGctQQqftyEJnJ49hNdOGf4jDNo61YYMlpDAr2g';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
