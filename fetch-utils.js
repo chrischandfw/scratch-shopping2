5e7b3c2ee7bc9204131c6f015d59bd55970b9f87
chris_chan <chrischandfw@gmail.com>
2022-03-29 13:29:46 -0500

    added two async functions to get shopping list and create item in list

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,7 +1,25 @@
 const SUPABASE_URL = '"https://wkkubdeijvrntdmunqer.supabase.co"';
-const SUPABASE_KEY = ''eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indra3ViZGVpanZybnRkbXVucWVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc1NTM1MTcsImV4cCI6MTk2MzEyOTUxN30.qE-NCGctQQqftyEJnJ49hNdOGf4jDNo61YYMlpDAr2g'';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indra3ViZGVpanZybnRkbXVucWVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc1NTM1MTcsImV4cCI6MTk2MzEyOTUxN30.qE-NCGctQQqftyEJnJ49hNdOGf4jDNo61YYMlpDAr2g';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function insertItem() {
+    const response = await client
+        .from('shopping_list')
+        .insert('item');
+
+    return response.body;
+}
+
+export async function getItems() {
+    const response = await client
+        .from('shopping_list')
+        .select('*');
+
+    return response.body;
+}
+
+
 
 export function getUser() {
     return client.auth.session() && client.auth.session().user;
