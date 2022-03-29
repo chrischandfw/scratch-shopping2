b3d09eda171ebd271f1dbbb4f35bdc514a4a3665
chris_chan <chrischandfw@gmail.com>
2022-03-29 15:12:16 -0500

    grabbed dom elements and added shopping form event listener

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,9 +1,36 @@
-import { checkAuth, logout } from '../fetch-utils.js';
+import { 
+    checkAuth, 
+    logout,
+    createItem,
+    getItems,
+    deleteAllItems,
+    buyItem,
+} from '../fetch-utils.js';
+
+import { renderItem } from '../render-utils.js';
 
 checkAuth();
 
+const shoppingForm = document.querySelector('.item-form');
+const deleteButton = document.querySelector('.delete-button');
+const shoppingListEl = document.querySelector('.shopping-list');
 const logoutButton = document.getElementById('logout');
 
 logoutButton.addEventListener('click', () => {
     logout();
+});
+
+shoppingForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(shoppingForm);
+
+    await createItem({
+        item: data.get('item'),
+        amount: data.get('amount'),
+        is_bought: false,
+    });
+
+    shoppingForm.reset();
+
+	//await fetchAndDisplayList();
 });
